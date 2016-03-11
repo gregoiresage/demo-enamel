f3bb394f00dd358d81edeacc9898c123ad22d7a0
Grégoire Sage <gregoire.sage@gmail.com>
2016-03-12 00:57:39 +0100

    update demo

--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@
 #include "enamel.h"
 
 static Window *window;
-static TextLayer *text_layer;
-static TextLayer *text_layer_2;
+static TextLayer *text_layer_title;
+static TextLayer *text_layer_food;
+static TextLayer *text_layer_drink;
 
 static char favorite_food_string[100];
 
@@ -22,11 +23,11 @@ static void updateDisplay() {
   else {
     window_set_background_color(window,GColorWhite);
   }
-  
+
   switch(get_font_size()){
-    case FONT_SIZE_SMALL : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14)); break;
-    case FONT_SIZE_NORMAL : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)); break;
-    case FONT_SIZE_LARGE : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD)); break;
+    case FONT_SIZE_SMALL : text_layer_set_font(text_layer_title, fonts_get_system_font(FONT_KEY_GOTHIC_14)); break;
+    case FONT_SIZE_NORMAL : text_layer_set_font(text_layer_title, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)); break;
+    case FONT_SIZE_LARGE : text_layer_set_font(text_layer_title, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD)); break;
   }
 
   favorite_food_string[0] = '\0';
@@ -35,7 +36,12 @@ static void updateDisplay() {
     p = mystrcat(p,get_favorite_food(i));
     p = mystrcat(p,"\n");
   }
-  text_layer_set_text(text_layer_2, favorite_food_string);
+  text_layer_set_text(text_layer_food, favorite_food_string);
+
+  text_layer_set_text(text_layer_drink, get_favorite_drink());
+
+  APP_LOG(0, "email is %s", get_email());
+  
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
@@ -46,25 +52,31 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(bounds);
-  text_layer_set_text(text_layer, "Change the background in the configuration page");
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  text_layer_title = text_layer_create(GRect(0,0,bounds.size.w,30));
+  text_layer_set_text(text_layer_title, "Enamel Demo");
+  text_layer_set_background_color(text_layer_title, GColorClear);
+  text_layer_set_text_alignment(text_layer_title, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer_title));
 
-  bounds.origin.y = 120;
-  text_layer_2 = text_layer_create(bounds);
-  text_layer_set_text(text_layer_2, favorite_food_string);
-  text_layer_set_background_color(text_layer_2, GColorClear);
-  text_layer_set_text_alignment(text_layer_2, GTextAlignmentLeft);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer_2));
+  text_layer_food = text_layer_create(GRect(0,40,bounds.size.w,100));
+  text_layer_set_text(text_layer_food, favorite_food_string);
+  text_layer_set_background_color(text_layer_food, GColorClear);
+  text_layer_set_text_alignment(text_layer_food, GTextAlignmentLeft);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer_food));
+
+  text_layer_drink = text_layer_create(GRect(0,40,bounds.size.w,100));
+  text_layer_set_text(text_layer_drink, favorite_food_string);
+  text_layer_set_background_color(text_layer_drink, GColorClear);
+  text_layer_set_text_alignment(text_layer_drink, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer_drink));
 
   updateDisplay();
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
-  text_layer_destroy(text_layer_2);
+  text_layer_destroy(text_layer_title);
+  text_layer_destroy(text_layer_food);
+  text_layer_destroy(text_layer_drink);
 }
 
 static void init(void) {
